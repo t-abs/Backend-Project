b19d9840d2c4b34ac579d222b1987647606c62de
Tanisha Basu <137747688+t-abs@users.noreply.github.com>
2025-05-21 11:34:09 +0530

    Update File.js

--- models/File.js
+++ models/File.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer=require("nodemailer");
 
 const fileSchema = new mongoose.Schema({
   name: {
@@ -15,5 +16,35 @@ const fileSchema = new mongoose.Schema({
     type: String,
   },
 });
+
+//post middleware exports k phle
+
+fileSchema.post("save", async function (doc) {
+  try {
+    console.log('DOC:', doc);
+
+    // Create transporter
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+  //send mail
+  let info=await transporter.sendMail({
+    from:'Codehelp-By Babbar',
+    to:doc.mail,
+    subject:"New File Uploaded on Cloudinary",
+    html:'<h2>Hello Jee</h2> <p>File Uploaded</p>',
+  })
+  console.log("INFO",info);
+
+}
+  catch(error){
+    console.error(error);
+  }
+});
+
 
 module.exports = mongoose.model("File", fileSchema);
